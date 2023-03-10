cc813f06be431274f60441ed25c076d1309faddc
Edward Bates <gitmux@stemux.com>
2023-03-10 17:53:41 -0600

    packmux.c: Write result of curl to file
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- packmux.c
+++ packmux.c
@@ -2,10 +2,25 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
+size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata)
+{
+	size_t realsize = (size * nmemb);
+	FILE *fp = (FILE *)userdata;
+	fwrite(ptr, size, nmemb, fp);
+	return realsize;
+}
+
 int main() {
 	CURL* curl;
 	CURLcode res;
-	char* content = NULL;
+	FILE* fp;
+
+	fp = fopen("output.txt", "wb");
+
+	if(!fp) {
+		fprintf(stderr, "Failed to open file.\n");
+		exit(-1);
+	}
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	curl = curl_easy_init();
@@ -17,18 +32,19 @@ int main() {
 	curl_easy_setopt(curl, CURLOPT_URL, "https://api.github.com/repos/cmus/cmus/releases/latest");
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 	curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl");
-	/* curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL); */
-	/* curl_easy_setopt(curl, CURLOPT_WRITEDATA, &content); */
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
 	res = curl_easy_perform(curl);
+
 	if (res != CURLE_OK) {
 		fprintf(stderr, "Error fetching URL: %s\n", curl_easy_strerror(res));
 		curl_easy_cleanup(curl);
-		free(content);
 		return 1;
 	}
 
-
-	free(content);
+	curl_global_cleanup();
+	fclose(fp);
 
 	return 0;
 }
